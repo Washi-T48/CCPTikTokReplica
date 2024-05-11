fdefe0d6c303b93a2b4d78a76dec6d52b7789e30
Pichayut Sombat <psb.nnt@gmail.com>
2024-05-11 18:24:40 +0700

    Add thing

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,59 @@ import { Buttons } from './Buttons'
 import './App.css'
 import goblinvid from '../assets/goblin.mp4'
 import confusedvid from '../assets/confused.mp4'
+import jazzvid from '../assets/jazz.mp4'
+
+// DUMMY DATA //
+
+let confused = {
+  video: confusedvid,
+  desc: {
+    vidName: ' ConfusedKhmer69',
+    vidDesc: 'อ้าวทุกคน อันนี้งูนะมันจะอมใข่ได้ยังไงน่ะ ตัวมันนิดเดียวเองเห็นมะ ผมว่าไม่น่าจะอมได้นะเว้ยนิ อย่าเข้าใกล้นะ เด็กเล็กนะ อันตราย โอ๊ะ ปากมันกว้างขนาดนั้นได้เชียวเหรอน่ะ อมไปได้ยังไงน่ะ ไม่อยากจะเชื่อเลย',
+    media: 'ConfusedKhmer69\'s original sound'
+  },
+  data: {
+    isLiked: false,
+    likes: 1000,
+    comments: 200,
+  }
+}
+
+let goblin = {
+  video: goblinvid,
+  desc: {
+    vidName: ' JapaneseGoblin420',
+    vidDesc: 'Whew, finally finished it! this is actually my first time to ever make a PV like this, if i\'m being honest Never really thought that i\'ve managed to finished this thing. Overall it took me about 3 weeks to make this',
+    media: 'Goburin'
+  },
+  data: {
+    isLiked: true,
+    likes: 100,
+    comments: 20,
+  }
+}
+
+let jazz = {
+  video: jazzvid,
+  desc: {
+    vidName: 'REYSON NEKRUTMAN',
+    vidDesc: 'I proudly endorse Pearl Drums, Meinl Cymbals, Remo Drumheads, and Vic Firth Drumsticks',
+    media: '@GreysonNekrutmandrums'
+  },
+  data: {
+    isLiked: true,
+    likes: 21,
+    comments: 20,
+  }
+}
+
+// DUMMY DATA //
+
+const videoData = [confused, goblin, jazz]
+
 
 class App extends Component {
   render() {
-
-    // DUMMY DATA //
-
-    let confused = {
-      video: confusedvid,
-      desc: {
-        vidName: ' ConfusedKhmer69',
-        vidDesc: 'อ้าวทุกคน อันนี้งูนะมันจะอมใข่ได้ยังไงน่ะ ตัวมันนิดเดียวเองเห็นมะ ผมว่าไม่น่าจะอมได้นะเว้ยนิ อย่าเข้าใกล้นะ เด็กเล็กนะ อันตราย โอ๊ะ ปากมันกว้างขนาดนั้นได้เชียวเหรอน่ะ อมไปได้ยังไงน่ะ ไม่อยากจะเชื่อเลย',
-        media: 'ConfusedKhmer69\'s original sound'
-      },
-      data: {
-        isLiked: false,
-        likes: 1000,
-        comments: 200,
-        shares: 100
-      }
-    }
-
-    let goblin = {
-      video: goblinvid,
-      desc: {
-        vidName: ' JapaneseGoblin420',
-        vidDesc: 'Whew, finally finished it! this is actually my first time to ever make a PV like this, if i\'m being honest Never really thought that i\'ve managed to finished this thing. Overall it took me about 3 weeks to make this',
-        media: 'Goburin'
-      },
-      data: {
-        isLiked: true,
-        likes: 100,
-        comments: 20,
-        shares: 10
-      }
-    }
-
-    // DUMMY DATA //
 
     const videoSelector = confused
 
